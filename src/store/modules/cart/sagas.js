3e6fe997d99b4982caf8c98826cd7f8ab566c5a3
Nilomiranda <nilomiranda3@gmail.com>
2019-09-03 22:33:06 -0300

    Checking stock when updating item quantity inside cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,7 @@
 import { call, put, all, select, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
@@ -27,7 +27,7 @@ function* addToCart({ id }) {
 
   if (productExists) {
     // const amount = productExists.amount + 1;
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -41,4 +41,21 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateCart({ id, amount }) {
+  if (amount < 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Out of stock');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateCart),
+]);
